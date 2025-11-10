4e19f54e8b4517066aa10cb66d1042e824324435
lorne <1991wangliang@gmail.com>
2025-11-10 13:55:40 +0800

    fix ui

--- web/src/pages/ccbz.tsx
+++ web/src/pages/ccbz.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styles from './ccbz.modules.css';
-import { DatePicker, Form, Input, Modal, Radio } from "antd";
+import { Col, DatePicker, Form, Input, Modal, Radio, Row } from "antd";
 import { numberToChineseRMB } from "../utils/money";
 import dayjs from "dayjs";
 
@@ -34,46 +34,70 @@ const CCBZPage = () => {
                     setVisible(false);
                 }}
             >
-                <Form form={form}>
-                    <Form.Item name="name" label="姓名">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item name="department" label="部门">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item name="project" label="申请项目">
-                        <Radio.Group>
-                            <Radio value="1">市内出差</Radio>
-                            <Radio value="2">省内出差</Radio>
-                            <Radio value="3">省外出差</Radio>
-                            <Radio value="4">其他</Radio>
-                        </Radio.Group>
-                    </Form.Item>
-                    <Form.Item name="address" label="出差地点">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item name="date" label="预计出差时间">
-                        <DatePicker.RangePicker />
-                    </Form.Item>
-                    <Form.Item name="days" label="共几天">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item name="projectName" label="项目名称">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item name="currentPeople" label="出差人数">
-                        <Input type="number" />
-                    </Form.Item>
-                    <Form.Item name="currentDays" label="出差天数">
-                        <Input type="number" />
-                    </Form.Item>
-                    <Form.Item name="currentMoneyPerDay" label="每人每天多少钱">
-                        <Input type="number" />
-                    </Form.Item>
-                    <Form.Item name="money" label="出差补贴费用总计">
-                        <Input type="number" />
-                    </Form.Item>
 
+                <Form form={form}>
+                    <Row gutter={[12, 12]}>
+                        <Col span={12}>
+                            <Form.Item name="name" label="姓名">
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item name="department" label="部门">
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={24}>
+                            <Form.Item name="projectName" label="项目名称">
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={24}>
+                            <Form.Item name="project" label="申请项目">
+                                <Radio.Group>
+                                    <Radio value="1">市内出差</Radio>
+                                    <Radio value="2">省内出差</Radio>
+                                    <Radio value="3">省外出差</Radio>
+                                    <Radio value="4">其他</Radio>
+                                </Radio.Group>
+                            </Form.Item>
+                        </Col>
+                        <Col span={8}>
+                            <Form.Item name="date" label="出差时间">
+                                <DatePicker.RangePicker />
+                            </Form.Item>
+                        </Col>
+                        <Col span={8}>
+                            <Form.Item name="address" label="出差地点">
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={8}>
+                            <Form.Item name="days" label="共几天">
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={6}>
+                            <Form.Item name="currentPeople" label="出差人数">
+                                <Input type="number" />
+                            </Form.Item>
+                        </Col>
+                        <Col span={6}>
+                            <Form.Item name="currentDays" label="出差天数">
+                                <Input type="number" />
+                            </Form.Item>
+                        </Col>
+                        <Col span={6}>
+                            <Form.Item name="currentMoneyPerDay" label="每人每天多少钱">
+                                <Input type="number" />
+                            </Form.Item>
+                        </Col>
+                        <Col span={6}>
+                            <Form.Item name="money" label="出差补贴费用总计">
+                                <Input type="number" />
+                            </Form.Item>
+                        </Col>
+                    </Row>
                 </Form>
             </Modal>
 
@@ -193,7 +217,7 @@ const CCBZPage = () => {
                     </td>
                 </tr>
             </table>
-        </div>
+        </div >
     )
 }
 
